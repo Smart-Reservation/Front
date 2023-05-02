68c732e9b5f3e1b5e6c1dc30fa325ee31569acb8
kimsuyeon0916 <5667649@naver.com>
2023-05-02 17:47:30 +0900

    feat: change click eventhandler

--- src/components/Period.js
+++ src/components/Period.js
@@ -1,47 +1,44 @@
-import {  useState } from "react";
+import { useState, useEffect } from "react";
 import styled, { css } from "styled-components";
 
 //styled-components
 
-const Container = styled.div`
-`;
+const Container = styled.div``;
 const ContentBox = styled(Container)`
-  ${(props)=>
-    props.clicked
-      ? css`
-        `
-      : css`
-          display: flex;
-          flex-direction: row;
-          align-items: flex-start;
-          padding: 4px 16px;
-          gap: 8px;
-          
-          width: 459px;
-          height: 32px;
-          
-          
-          /* Inside auto layout */
-          
-          flex: none;
-          order: 4;
-          align-self: stretch;
-          flex-grow: 0;
-        `
-  }
+  /* Auto layout */
+
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  padding: 4px 16px;
+  gap: 8px;
+
+  width: 459px;
+  height: 32px;
+
+  background: ${(props) => (props.clicked ? "#E0E2E6" : "white")};
+  text-align: center;
+
+  /* Inside auto layout */
+
+  flex: none;
+  order: 0;
+  align-self: stretch;
+  flex-grow: 0;
+  ${(props)=>props.clicked?"":`&:hover {
+    background-color: rgba(55, 53, 47, 0.05);
+  }`}
 `;
 
-function Period({period}){
-  const [clicked, setClicked] = useState(false);
-
-    return (
-      <Container>
-        <ContentBox clicked={clicked}>
-          <div>
-            {period.startTime} ~ {period.endTime}
-          </div>
-        </ContentBox>
-      </Container>
-    )
+function Period({ period, onClick, index, clicked }) {
+  return (
+    <Container onClick={()=>onClick(index)}>
+      <ContentBox clicked={clicked} >
+        <div>
+          {period.startTime} ~ {period.endTime}
+        </div>
+      </ContentBox>
+    </Container>
+  );
 }
 export default Period;
--- src/components/PeriodList.js
+++ src/components/PeriodList.js
@@ -1,8 +1,12 @@
 import Period from "./Period";
-import { useReservationInfoState, useReservationInfoDispatch } from '../context/ReservationInfoContext';
-import styled ,{css}  from "styled-components";
+import {
+  useReservationInfoState,
+  useReservationInfoDispatch,
+} from "../context/ReservationInfoContext";
+import styled, { css } from "styled-components";
+import { useState, useEffect } from "react";
 
-const PeriodListContainer=styled.div`
+const PeriodListContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
@@ -17,25 +21,41 @@ const PeriodListContainer=styled.div`
 
   /* White */
 
-  background: #FFFFFF;
-  box-shadow: 0px 30px 84px rgba(19, 10, 46, 0.08), 0px 8px 32px rgba(19, 10, 46, 0.07), 0px 3px 14px rgba(19, 10, 46, 0.03), 0px 1px 3px rgba(19, 10, 46, 0.13);
+  background: #ffffff;
+  box-shadow: 0px 30px 84px rgba(19, 10, 46, 0.08),
+    0px 8px 32px rgba(19, 10, 46, 0.07), 0px 3px 14px rgba(19, 10, 46, 0.03),
+    0px 1px 3px rgba(19, 10, 46, 0.13);
   border-radius: 8px;
 `;
+const Box = styled.div``;
 
-function PeriodList({periods}){
+function PeriodList({ periods }) {
   const state = useReservationInfoState();
-  const dispatch = useReservationInfoDispatch(); 
-  
-  return(
+  const dispatch = useReservationInfoDispatch();
+  const [clickeds, setClickeds] = useState(Array(periods.length).fill(false));
+
+  const onClick = (index) => {
+    const newArr = Array(periods.length).fill(false);
+    newArr[index] = true;
+    setClickeds(newArr);
+  };
+
+  // const notClicked=(index)=>{
+  //   clickeds[index]=false;
+  // }
+
+  return (
     <PeriodListContainer>
-      {periods.map((period)=>(
-        <Period period={period}
+      {periods.map((period, index) => (
+        <Period
+          onClick={onClick}
+          period={period}
+          index={index}
+          clicked={clickeds[index]}
         />
       ))}
     </PeriodListContainer>
-  )
-
+  );
 }
 
 export default PeriodList;
-
