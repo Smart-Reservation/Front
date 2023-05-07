8563e5c92823bfbff83b339f8673525c408cab3f
goder-0 <goder0@naver.com>
2023-05-08 04:09:48 +0900

    refactor: Calendar Component Refactoring

--- src/components/calender/Calendar.js
+++ src/components/calender/Calendar.js
@@ -1,124 +1,150 @@
 import Day from "./days";
 import { useEffect, useRef, useState } from "react";
-import "./calender.css";
+import styled from "styled-components";
 // import Head from './header';
 
+const CalendarContainer = styled.div`
+  font-family: "Montserrat";
+  font-style: normal;
+  font-weight: 300;
+  font-size: 20px;
+  line-height: 3em;
+  text-align: center;
+
+  margin-top:20px;
+  display: inline-flex;
+  justify-content: center;
+  flex-direction: column;
+  text-align: center;
+  border: 1PX solid rgb(253, 234, 234);
+  border-radius: 5%;
+  padding: 3vw;
+`
+
+const CalendarHeader = styled.div`
+  display:flex;
+  -webkit-box-pack: center;
+  justify-content: space-between;
+`
+
+const WeekContainer = styled.div`
+  display:flex;
+  flex-direction: row;
+  justify-content: center;
+
+`
+
+const MonthButton = styled.button`
+  font-size:1.5em;
+  border:0px;
+  background-color: white;
+  color:black;
+  cursor:pointer;
+
+`
+const WEEKDAYS = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+//현재보다 이전이면 true
+function dateLTnow(date,now){
+  if(date<=now){
+    return date.getFullYear()===now.getFullYear()&&date.getMonth()===now.getMonth()?date.getDate()<now.getDate():true;
+  }
+  return false;
+}
+
+function getDates(startDate, endDate) {
+  const dates = [];
+  for (let i = 0; i < startDate.getDay(); i++) dates.push('');
+
+  for (let date = startDate; date <= endDate; date.setDate(date.getDate() + 1)) {
+    dates.push(new Date(date));
+  }
+  while (dates.length % 7 !== 0) { dates.push(''); }
+
+  return dates;
+}
+
 function Calender() {
-    let now = new Date();
-    let month = now.getMonth() + 1;
-    let year = now.getFullYear();
-    const [monthCurrent, setMonth] = useState(month);
-    const [yearCurrent, setyear] = useState(year);
-    const monthCount = useRef(month);
-    const yearCount = useRef(year);
-    const [data, setData] = useState();
+  let now = new Date();
+  const [year, setyear] = useState(now.getFullYear());
+  const [month, setMonth] = useState(now.getMonth() + 1);
+  const [fullMonth, setFullMonth] = useState([]);
+  const [selectedDate, setSelectedDate] = useState();
+  const [data, setData] = useState();
 
-    const onClick = (d) => {
-        setData(d);
-        console.log(data);
-    }
-    const onClickNo = () => {
-        console.log('')
-    }
+  const handleDayClick = (date) => {
+    setSelectedDate(date);
+  };
 
-    function monthAfter() {
-        if (monthCount.current === 12) {
-            monthCount.current = 0;
-            yearCount.current++;
-            setyear(yearCount.current)
-            console.log(startDayCal.yearFirstDay);
-        }
-        monthCount.current++;
-        setMonth(monthCount.current);
-    }
+  const handleEmptyClick = () => {
+    console.log("");
+  };
 
-    function monthBefore() {
-        if (monthCount.current === 1) {
-            monthCount.current = 13;
-            yearCount.current--;
-            setyear(yearCount.current);
+  function nextMonth() {
+    setyear(year + parseInt(month / 12))
+    setMonth(month % 12 + 1);
+    setSelectedDate()
+  }
 
-        }
-        monthCount.current--;
+  function previousMonth() {
+    setMonth((month + 10) % 12 + 1);
+    setyear(year - parseInt(((month + 10) % 12 + 1) / 12))
+    setSelectedDate()
 
-        setMonth(monthCount.current);
+  }
 
-        // 2016:5,
-        // 2017:0,
-        // 2018:1,
-        // 2019:2,
-        // 2020:3,
-        // 2021:5,
-        // 2022:6,
-        // 2023:0,
-        // 2024:
-    }
-    const yearFirstDay = {};
-    const startDayCal = () => {
-        let day = 6;
-        for (let year = 2000; year <= 2100; year++) {
-            yearFirstDay[year] = day;
-            day++;
-            if (year % 4 === 0) day += 1;
-            if (day === 7) day = 0;
-            if (day === 8) day = 1;
-        }
-        console.log(yearFirstDay)
-    }
-    startDayCal();
+  useEffect(() => {
+    const startDate = new Date(year, month - 1, 1);
+    const endDate = new Date(year, month, 0);
+    const dates = getDates(startDate, endDate);
+    const weeks = [[]];
 
 
-    function fullYearAdder(startDate) {
-        const fullYear = [];
-        const monthForYear = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-        let sevenDays = [];
-        for (let i = 0; i < startDate; i++) {
-            sevenDays.push(<Day date={''} month={0} key={i} monthCompare={monthCount.current} onClick={onClickNo} />);
-        }
-        for (let j = 0; j <= monthForYear.length; j++)
-            for (let i = 1; i <= monthForYear[j]; i++) {
-                sevenDays.push(<Day date={i} month={j + 1} key={i} monthCompare={monthCount.current} onClick={onClick} />);
-                if (sevenDays.length == 7) {
-                    fullYear.push(sevenDays);
-                    sevenDays = [];
-                }
-            }
-        return fullYear;
-    }
+    dates.forEach((date) => {
+      if (weeks.length === 0 || weeks[weeks.length - 1].length === 7) {
+        weeks.push([]);
+      }
+      weeks[weeks.length - 1].push(date);
+    });
 
-    function DaySpliter({ month }) {
-        const a = fullYearAdder(yearFirstDay[yearCurrent]);
-        const b = [];
+    const mon = weeks.map((week, index) => (
+      <WeekContainer key={index + 'week'}>
+        {week.map((date, idx) => (
+          <Day
+            date={date}
+            onClickEmpty={handleEmptyClick}
+            onClickDay={() => handleDayClick(date)}
+            key={typeof (date) === 'string' ? index + date + idx : date.getDate()}
+            isDate={typeof (date) !== 'string'}
+            selectedDate={selectedDate}
+            disable={typeof (date) !== 'string' ?
+            dateLTnow(date,now) : 
+            false}
+          />
+        ))}
+      </WeekContainer>
+    ));
 
-        for (let i = 0; i < a.length; i++) {
-            if (a[i][0].props.month === month || a[i][a[i].length - 1].props.month === month) {
-                b.push(<div className="oneWeek">{a[i]}</div>);
-            }
-        }
-        return <div>{b}</div>;
-    }
-
-    function DayOfTheWeek() {
-        const daylist = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-
-        return <div className="oneWeek">
-            {daylist.map((value) =>
-                <Day date={value} onClick={onClickNo} />
-            )}
-        </div>
-    }
+    setFullMonth(mon);
+  }, [year, month, selectedDate]);
 
 
-    return (
-        <>
-            {/* <Header /> */}
-            <div className="calender" >
-                <div ><button onClick={monthBefore}>&lt;</button><span>{yearCurrent}년{monthCurrent}월</span> <button onClick={monthAfter}>&gt;</button></div>
-                <DayOfTheWeek />
-                <DaySpliter month={monthCurrent} />
-            </div>
-        </>
-    );
+  return (
+    <>
+      {/* <Header /> */}
+      <CalendarContainer>
+        <CalendarHeader>
+          <MonthButton onClick={previousMonth}>&lt;</MonthButton>
+          <span>{year}년{month}월</span>
+          <MonthButton onClick={nextMonth}>&gt;</MonthButton>
+        </CalendarHeader>
+        <WeekContainer>
+          {WEEKDAYS.map((value) => <Day key={value} date={value} onClick={handleEmptyClick} />)}
+        </WeekContainer>
+        {fullMonth}
+      </CalendarContainer>
+    </>
+  );
 }
 
 export default Calender;
