5d346665f698f9009b65cb8219387400a04e9de1
Kwon <ggiicchh48@gmail.com>
2023-05-07 16:25:43 +0900

    calendar is now perfecto

--- src/components/calender/Calendar.js
+++ src/components/calender/Calendar.js
@@ -7,11 +7,12 @@ function Calender(){
     let now=new Date();
     let month=now.getMonth();
     let year= now.getFullYear();
-    const monthCount=useRef(month);
     const [monthCurrent,setMonth]= useState(month);
+    const [yearCurrent,setyear]= useState(year);
+    const monthCount=useRef(month);
+    const yearCount=useRef(year);
     const [data, setData] = useState();
-
-
+    
     const onClick=(d)=>{
         setData(d);
         console.log(data);
@@ -21,25 +22,57 @@ function Calender(){
     }
 
     function monthAfter(){
-
-        setMonth(monthCount.current+1);
+        if(monthCount.current===12){
+            monthCount.current=0;
+            yearCount.current++;
+            setyear(yearCount.current)
+            console.log(startDayCal.yearFirstDay);
+        }
         monthCount.current++;
+        setMonth(monthCount.current);
 
     }
     function monthBefore(){
-        setMonth(monthCount.current-1);
+        if(monthCount.current===1){
+            monthCount.current=13;
+            yearCount.current--;
+            setyear(yearCount.current);
+            
+        }
         monthCount.current--;
-    }
 
+        setMonth(monthCount.current);
+
+        // 2016:5,
+        // 2017:0,
+        // 2018:1,
+        // 2019:2,
+        // 2020:3,
+        // 2021:5,
+        // 2022:6,
+        // 2023:0,
+        // 2024:
+    }
+    const yearFirstDay={};
+    const startDayCal=()=>{
+        let day=6;
+        for(let year=2000; year<=2100; year++){
+            yearFirstDay[year]=day;
+            day++;
+            if(year%4===0) day+=1;
+            if(day===7 ) day=0;
+            if(day===8) day=1;}    
+    console.log(yearFirstDay)
+}
+    startDayCal();
     
 
     function fullYearAdder (startDate){
         const fullYear=[];
         const monthForYear=[31,28,31,30,31,30,31,31,30,31,30,31];
         let sevenDays=[];
-        console.log('반복 확인')
-        for(let i=0; i<startDate; i++){
-            sevenDays.push('');}
+         for(let i=0; i<startDate; i++){
+            sevenDays.push(<Day date={''} month={0} key={i} monthCompare={monthCount.current}  onClick={onClickNo}/>);}
             for(let j=0; j<=monthForYear.length; j++)
                 for(let i=1; i<=monthForYear[j]; i++){
                     sevenDays.push(<Day date={i} month={j+1} key={i} monthCompare={monthCount.current}  onClick={onClick}/>);
@@ -51,7 +84,9 @@ function Calender(){
                 }
 
     function DaySpliter({month}){
-        const a= fullYearAdder();
+        console.log(month)
+
+        const a= fullYearAdder(yearFirstDay[yearCurrent]);
         const b=[];
         console.log(a)
         
@@ -72,9 +107,7 @@ function Calender(){
 
 
     function DayOfTheWeek(){
-        const daylist=["일","월","화","수","목","금","토"];
-
-        
+        const daylist=["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
 
         return <div className="oneWeek">
             {daylist.map((value)=>
@@ -86,11 +119,12 @@ function Calender(){
    
     return(
         <>
+
         {/* <Head /> */}
         <div className="calender" > 
-            <div ><button onClick={monthBefore}>&lt;</button><span>{year}년{monthCurrent}월</span> <button onClick={monthAfter}>&gt;</button></div>
+            <div ><button onClick={monthBefore}>&lt;</button><span>{yearCurrent}년{monthCurrent}월</span> <button onClick={monthAfter}>&gt;</button></div>
             <DayOfTheWeek/>
-            <DaySpliter  month={monthCount.current}/>
+            <DaySpliter  month={monthCurrent}/>
         </div>
         </>
     )
