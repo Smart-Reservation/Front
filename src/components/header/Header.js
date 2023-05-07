6a24c72a2a45acb742ca52c833b8f5c0ef9e393e
goder-0 <goder0@naver.com>
2023-05-07 17:24:17 +0900

    refactor: rename Header Name

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,48 +1,28 @@
-import logo from "./images/logo.png";
-import "./header.css";
-import profile from "./images/profile.png";
-import profileBoss from "./images/profile_boss.png";
-import { useState } from "react";
+import logo from './images/logo.png';
+import './header.css';
+import profile from './images/profile.png';
+import profileBoss from './images/profile_boss.png';
+import { useState } from 'react';
 import login from '../Login';
+
+
 function Header() {
-  const [active, setActive] = useState(false);
-  let button;
-  function alterner() {
-    active ? setActive(false) : setActive(true);
-  }
+        const [active, setActive] = useState(false);
+        let button;
+        function alterner() { active ? setActive(false) : setActive(true); }
 
-  button = active ? (
-    <div className="bossTog">
-      <img src={profileBoss} />
-    </div>
-  ) : (
-    <div className="customerTog">
-      <img src={profile} />
-    </div>
-  );
+        button = active ? <div className='bossTog' ><img src={profileBoss} /></div> : <div className='customerTog' ><img src={profile} /></div>
 
-  return (
-    <div className="top">
-      <div>
-        <img id="logo" src={logo} alt="logo"></img>
-      </div>
-
-      <div className="rightSide">
-        <button className="btnHeader" id="btnOut" type="button" onClick={login}>
-          Connect Binance Wallet
-          <button
-            className="btnHeader"
-            id="toggle"
-            type="button"
-            isActive={active}
-            onClick={alterner}
-          >
-            {button}{" "}
-          </button>
-        </button>
-      </div>
-    </div>
-  );
+        return (
+                <div className='top'>
+                        <div><img id="logo" src={logo} alt="logo"></img></div>
+                        <div className='rightSide'>
+                                <button className='btnHeader' id="btnOut" type='button' onClick={login}>Connect Binance Wallet
+                                        <button className='btnHeader' id="toggle" type='button' isActive={active} onClick={alterner} >{button} </button>
+                                </button>
+                        </div>
+                </div>
+        )
 }
 
-export default Header;
+export default Header;                    
