d7d6db6d27b2f1ab8b2b032f61aaa5fb8608f1e6
goder-0 <goder0@naver.com>
2023-05-08 05:40:11 +0900

    feat: Logo Link to MainPage

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,10 @@
 import logo from './images/logo.png';
 import './header.css';
-import profile from './images/profile.png';
-import profileBoss from './images/profile_boss.png';
+import Profile from './images/profile.png';
+import ProfileStore from './images/profile_boss.png';
 import { useState } from 'react';
 import login from '../Login';
+import { Link } from 'react-router-dom';
 
 
 function Header() {
@@ -11,14 +12,14 @@ function Header() {
         let button;
         function alterner() { active ? setActive(false) : setActive(true); }
 
-        button = active ? <div className='bossTog' ><img src={profileBoss} /></div> : <div className='customerTog' ><img src={profile} /></div>
+        button = active ? <div className='bossTog' ><img src={ProfileStore} alt='profileStore'/></div> : <div className='customerTog' ><img src={Profile}  alt='profile'/></div>
 
         return (
                 <div className='top'>
-                        <div><img id="logo" src={logo} alt="logo"></img></div>
+                        <Link to={'/'}><img id="logo" src={logo} alt="logo"></img></Link>
                         <div className='rightSide'>
                                 <button className='btnHeader' id="btnOut" type='button' onClick={login}>Connect Binance Wallet
-                                        <button className='btnHeader' id="toggle" type='button' isActive={active} onClick={alterner} >{button} </button>
+                                        <button className='btnHeader' id="toggle" type='button' isactive={active.toString()} onClick={alterner} >{button} </button>
                                 </button>
                         </div>
                 </div>
