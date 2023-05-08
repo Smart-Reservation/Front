6e599d26cdf09bdea6de8bd609b674811962bae2
kimsuyeon0916 <5667649@naver.com>
2023-05-08 11:18:59 +0900

    feat: modify header css

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,8 +2,9 @@ import logo from "./images/logo.png";
 import "./header.css";
 import profile from "./images/profile.png";
 import profileBoss from "./images/profile_boss.png";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import login from '../Login';
+import UserOption from "../user_option/UserOption";
 function Header() {
   const [active, setActive] = useState(false);
   let button;
@@ -20,16 +21,27 @@ function Header() {
       <img src={profile} />
     </div>
   );
+  const [logged, setlogged]=useState(false); //전역적으로 관리
+  const [clicked,setClicked]=useState(false);
 
+  const onClick=()=>{
+    setClicked((clicked)=>!clicked);
+  }
+  const loggedIn=()=>{
+    login();
+    setlogged(true);
+  }
   return (
     <div className="top">
       <div>
         <img id="logo" src={logo} alt="logo"></img>
       </div>
-
       <div className="rightSide">
-        <button className="btnHeader" id="btnOut" type="button" onClick={login}>
-          Connect Binance Wallet
+        <button className="btnHeader" id="btnOut" type="button" >
+          <div 
+            className="btnHeader"
+            id="wallet"
+            onClick={logged?onClick: loggedIn}>Connect Binance Wallet</div>
           <button
             className="btnHeader"
             id="toggle"
@@ -41,6 +53,7 @@ function Header() {
           </button>
         </button>
       </div>
+      {clicked?<UserOption/>:<></>}  
     </div>
   );
 }
