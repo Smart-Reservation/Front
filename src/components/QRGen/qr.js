cc4c3267d5335eb63e3f8e153e11eaa59b6254c3
Kwon <ggiicchh48@gmail.com>
2023-05-09 19:05:23 +0900

    css is now styled-component

--- src/components/QRGen/qr.js
+++ src/components/QRGen/qr.js
@@ -1,41 +1,82 @@
 import React, { useState } from "react";
-import './qr.css'
-import ReactDOM from "react-dom";
 import QRCode from "react-qr-code";
+import styled from "styled-components";
 
+const QRContainer = styled.div`
+  display: block;
+  border: 10px sold black;
+  padding: 15% 10%;
+`;
 
+// const Hider = styled.div`
+//   display: "block";
+//   background: "black";
 
-function QR ({valueForQr}){
-    const [isCliked, setisCliked]= useState(true);
-    const onClick =()=>{
-        setisCliked(true)
-    }
+//   margin: "5%";
+//   width: "100%";
+//   height: "100%";
+//   padding: "5% 10%";
+//   borderradius: "10px";
+//   border: 1px solid black;
+// `;
 
+// const BtnHider = styled.div`
+//   position: absolute;
+//   border: 2px white solid;
+//   color: white;
+//   background-color: transparent;
+//   font-size: 2em;
 
-    const MakeQr= isCliked ? ()=>{return (<QRCode value={valueForQr} style={{background: 'white',
-margin:"5%", width:"100%" ,padding:"5% 10%", borderRadius:"10px"}} size={400} viewBox={`0 0 400 400`} />)
-    }: 
-    ()=>{return (<div id="hider"> <button type="button" id="btnHider"
-     onClick={onClick}>Don't let other see your QR</button></div>)}
+//   & :hover {
+//     cursor: grab;
+//   }
+// `;
 
-    // const HideQr=()=>{ return  (<div id="hider"> 
-    //     <MakeQr />
-    //     <button type="button" id="hider"> don't let others see it</button>
-    // </div>)};
-    
- 
-    return(
-        <div>
-        <div className="qrContainer" >
-        
-        <MakeQr/>
-        
+function QR({ valueForQr }) {
+  const [isCliked, setisCliked] = useState(true);
+
+  const onClick = () => {
+    setisCliked(isCliked ? false : true);
+  };
+
+  const MakeQr = isCliked
+    ? () => {
+        return (
+          <QRCode
+            value={valueForQr}
+            style={{
+              background: "white",
+              margin: "5%",
+              width: "100%",
+              padding: "5% 10%",
+              borderRadius: "10px",
+            }}
+            size={400}
+            viewBox={`0 0 400 400`}
+          />
+        );
+      }
+    : () => {
+        return "";
+        //   <Hider>
+        //     <BtnHider onClick={onClick}>Don't let other see it</BtnHider>
+        //   </Hider>
+      };
+
+  // const HideQr=()=>{ return  (<div id="hider">
+  //     <MakeQr />
+  //     <button type="button" id="hider"> don't let others see it</button>
+  // </div>)};
+
+  return (
+    <div>
+      <QRContainer>
+        <MakeQr />
+
         {/* <HideQr /> */}
-
-        </div>
-
-        </div>
-    )
+      </QRContainer>
+    </div>
+  );
 }
 
 export default QR;
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,113 @@
-import logo from './images/logo.png';
-import './header.css';
-import Profile from './images/profile.png';
-import ProfileStore from './images/profile_boss.png';
-import { useState } from 'react';
-import login from '../Login';
-import { Link } from 'react-router-dom';
+import logo from "./images/logo.png";
+import "./header.css";
+import Profile from "./images/profile.png";
+import ProfileStore from "./images/profile_boss.png";
+import { useState } from "react";
+import login from "../Login";
+import { Link } from "react-router-dom";
 import UserOption from "../user_option/UserOption";
+import styled from "styled-components";
+
+const Logo = styled.img`
+  height: 7em;
+  margin-left: 6em;
+`;
+
+const Top = styled.div`
+  height: 6em;
+  margin: 0px;
+  display: flex;
+  background-color: #c2c6cc;
+  justify-content: space-between;
+  width: 100vw;
+
+  font-family: "Montserrat";
+  font-style: normal;
+  font-weight: bold;
+`;
+const RightSide = styled.div`
+  display: flex;
+  align-items: center;
+  width: 30%;
+  justify-content: space-around;
+  margin-right: 5em;
+  flex-direction: column;
+`;
+const DivHeader = styled.div`
+  border-radius: 20%;
+  border: 1px black solid;
+  color: white;
+  box-shadow: 0px 30px 84px rgba(0, 0, 0, 0.08),
+    0px 8px 32px rgba(0, 0, 0, 0.07), 0px 3px 11px rgba(0, 0, 0, 0.03),
+    0px 1px 3px rgba(0, 0, 0, 0.13);
+  height: 3em;
+  background: #484848;
+  border-radius: 23px;
+  display: flex;
+  font-family: "Montserrat";
+  font-style: normal;
+  justify-content: space-between;
+  align-items: center;
+  &:hover {
+    cursor: grab;
+  }
+`;
+
+const BtnHeader = styled.button`
+  border-radius: 20%;
+  border: 1px black solid;
+  color: white;
+  box-shadow: 0px 30px 84px rgba(0, 0, 0, 0.08),
+    0px 8px 32px rgba(0, 0, 0, 0.07), 0px 3px 11px rgba(0, 0, 0, 0.03),
+    0px 1px 3px rgba(0, 0, 0, 0.13);
+  height: 3.5em;
+  background: #484848;
+  border-radius: 23px;
+  display: flex;
+  font-family: "Montserrat";
+  font-style: normal;
+  justify-content: space-between;
+  align-items: center;
+  &:hover {
+    cursor: grab;
+  }
+`;
+const Toggle = styled(BtnHeader)`
+  background-color: rgb(253, 234, 234);
+  border: transparent;
+  display: block;
+  width: 6em;
+  margin-right: -0.5em;
+
+  & > .customerTog > img {
+    height: 3em;
+    margin: 0;
+    padding-right: 70%;
+    transition: all 0.2s ease-in;
+  }
+  & > .bossTog > img {
+    height: 3em;
+    margin: 0;
+    padding-left: 40%;
+    transition: all 0.2s ease-in;
+  }
+`;
+const Wallet = styled(BtnHeader)`
+  border-radius: 23px;
+  width: 13em;
+  border: transparent;
+  /* border-radius: 20%; */
+  box-shadow: none;
+  padding-left: 1em;
+`;
+const BtnOut = styled(BtnHeader)`
+  border-radius: 23px;
+  width: 12em;
+  border: transparent;
+  /* border-radius: 20%; */
+  box-shadow: none;
+  padding-left: 1em;
+`;
 
 function Header() {
   const [active, setActive] = useState(false);
@@ -14,42 +116,45 @@ function Header() {
     active ? setActive(false) : setActive(true);
   }
 
-  button = active ? <div className='bossTog' ><img src={ProfileStore} alt='profileStore'/></div> : <div className='customerTog' ><img src={Profile}  alt='profile'/></div>
+  button = active ? (
+    <div className="bossTog">
+      <img src={ProfileStore} alt="profileStore" />
+    </div>
+  ) : (
+    <div className="customerTog">
+      <img src={Profile} alt="profile" />
+    </div>
+  );
 
+  const [logged, setlogged] = useState(false); //전역적으로 관리
+  const [clicked, setClicked] = useState(false);
 
-  const [logged, setlogged]=useState(false); //전역적으로 관리
-  const [clicked,setClicked]=useState(false);
-
-  const onClick=()=>{
-    setClicked((clicked)=>!clicked);
-  }
-  const loggedIn=()=>{
+  const onClick = () => {
+    setClicked((clicked) => !clicked);
+  };
+  const loggedIn = () => {
     login();
     setlogged(true);
-  }
+  };
   return (
-    <div className="top">
-      <Link to={'/'}><img id="logo" src={logo} alt="logo"></img></Link>
-      <div className="rightSide">
-        <button className="btnHeader" id="btnOut" type="button" >
-          <div 
-            className="btnHeader"
-            id="wallet"
-            onClick={logged?onClick: loggedIn}>Connect Binance Wallet</div>
-          <button
-            className="btnHeader"
-            id="toggle"
-            type="button"
-            isactive={active.toString()}
-            onClick={alterner}
-          >
-            {button}{" "}
-          </button>
-        </button>
-      </div>
-      {clicked?<UserOption/>:<></>}  
-    </div>
+    <Top>
+      <Link to={"/"}>
+        <Logo src={logo} alt="logo"></Logo>
+      </Link>
+      <RightSide>
+        <DivHeader>
+          <Wallet onClick={logged ? onClick : loggedIn}>
+            Connect Binance Wallet
+          </Wallet>
+
+          <Toggle type="button" isactive={active.toString()} onClick={alterner}>
+            {button}
+          </Toggle>
+        </DivHeader>
+      </RightSide>
+      {clicked ? <UserOption /> : <></>}
+    </Top>
   );
 }
 
-export default Header;                    
+export default Header;
