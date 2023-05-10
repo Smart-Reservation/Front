7c0d9899c1d4b8fcc4a153063e0e8a9308b367a5
kimsuyeon0916 <5667649@naver.com>
2023-05-11 08:05:38 +0900

    feat: modify reservaton detail

--- src/components/qr/generateQR/QrImage.js
+++ src/components/qr/generateQR/QrImage.js
@@ -3,9 +3,9 @@ import QRCode from "react-qr-code";
 import styled from "styled-components";
 
 const QRContainer = styled.div`
-  display: block;
+  display: flex;
   border: 10px sold black;
-  padding: 15% 10%;
+  padding: 5% 10%;
 `;
 
 // const Hider = styled.div`
@@ -32,14 +32,14 @@ const QRContainer = styled.div`
 //   }
 // `;
 
-function QR({ valueForQr }) {
-  const [isCliked, setisCliked] = useState(true);
+function QrImage({ valueForQr }) {
+  const [isClicked, setisClicked] = useState(true);
 
   const onClick = () => {
-    setisCliked(isCliked ? false : true);
+    setisClicked(!isClicked);
   };
 
-  const MakeQr = isCliked
+  const MakeQr = isClicked
     ? () => {
         return (
           <QRCode
@@ -48,11 +48,11 @@ function QR({ valueForQr }) {
               background: "white",
               margin: "5%",
               width: "100%",
-              padding: "5% 10%",
+              padding: "0% 10%",
               borderRadius: "10px",
             }}
-            size={400}
-            viewBox={`0 0 400 400`}
+            size={500}
+            // viewBox={`0 0 400 400`}
           />
         );
       }
@@ -69,14 +69,12 @@ function QR({ valueForQr }) {
   // </div>)};
 
   return (
-    <div>
       <QRContainer>
         <MakeQr />
 
         {/* <HideQr /> */}
       </QRContainer>
-    </div>
   );
 }
 
-export default QR;
+export default QrImage;
